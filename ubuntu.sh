5ce953ab1c36741c1f752e126ef34d5f2d4d266c
Justin Fuller <justindanielfuller@gmail.com>
2018-07-29 11:36:43 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,3 +3,13 @@ sudo apt-get install firefox
 sudo apt-get install git
 sudo apt-get install haskell-platform
 sudo apt-get install node
+sudo apt-get install xclip
+
+echo "Generating Github SSH key"
+ssh-keygen -t rsa -b 4096 -C "justindanielfuller@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+xclip -sel clip < ~/.ssh/id_rsa.pub
+echo "Github SSH Key copied to clipboard"
+echo "Opening github settings page in the default browser"
+xdg-open https://github.com/settings/keys
