ecf81f7835b1a880c2bbc5123291f174d653992e
Justin Fuller <justindanielfuller@gmail.com>
2018-07-30 16:01:51 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,6 +4,8 @@ sudo apt-get install git
 sudo apt-get install haskell-platform
 sudo apt-get install node
 sudo apt-get install xclip
+sudo apt-get install mysql-server
+sudo apt-get install postgresql postgresql-contrib
 
 echo "Generating Github SSH key"
 ssh-keygen -t rsa -b 4096 -C "justindanielfuller@gmail.com"
@@ -13,3 +15,8 @@ xclip -sel clip < ~/.ssh/id_rsa.pub
 echo "Github SSH Key copied to clipboard"
 echo "Opening github settings page in the default browser"
 xdg-open https://github.com/settings/keys
+
+cabal update
+cabal install hlint
+cabal install tasty-hunit
+cabal install sqlite-simple
