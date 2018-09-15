b39bb73e44c5173e044d88a18c7484f8a2183294
Justin Fuller <justindanielfuller@gmail.com>
2018-09-15 16:07:10 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -45,4 +45,7 @@ sudo update-rc.d mysql defaults
 
 # Vim related items
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+sudo apt-get install build-essential cmake # for youcompleteme
+sudo apt-get install python-dev python3-dev 
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
