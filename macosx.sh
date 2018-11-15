f3efc4821b8fbb430428e54b10fdfcb2f5fa5948
Justin Fuller <justindanielfuller@gmail.com>
2018-11-14 18:26:12 -0800

    postman installation

--- macosx.sh
+++ macosx.sh
@@ -16,6 +16,12 @@ rm -rf ./iTerm2-3_2_5.zip
 mv iTerm.app/ /Applications/
 spctl --add /Applications/iTerm.app/
 
+# Postman
+curl -O https://dl.pstmn.io/download/latest/osx
+unzip osx
+rm osx
+mv Postman.app /Applications/
+
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
